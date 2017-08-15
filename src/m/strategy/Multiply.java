110effe462aa8b33f7a4e313764abba053571e72
maomao <maomao.li@jjcargo.com>
2017-08-15 18:44:12 +0800

    add strategy

---
+++ src/m/strategy/Multiply.java
@@ -0,0 +1,13 @@
+package m.strategy;
+
+/**
+ * Created by maomao on 17/8/15.
+ */
+public class Multiply extends AbstractCalculator implements ICalculator {
+
+    @Override
+    public int calculate(String exp) {
+        int arrayInt[] = split(exp, "\\*");
+        return arrayInt[0] * arrayInt[1];
+    }
+}
---
+++ src/m/strategy/StrategyTest.java
@@ -0,0 +1,14 @@
+package m.strategy;
+
+/**
+ * Created by maomao on 17/8/15.
+ */
+public class StrategyTest {
+    public static void main(String[] args) {
+        String exp = "2+8";
+        ICalculator cal = new Plus();
+        int result = cal.calculate(exp);
+        System.out.println(result);
+    }
+
+}
